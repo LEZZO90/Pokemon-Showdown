de0853aef2210517edecc8d2a7b3a091560f6a75
LEZZO90 <77779987nm@hm>
2015-04-06 16:00:25 +0200

    Update misc.js

--- funstuff/misc.js
+++ funstuff/misc.js
@@ -66,6 +66,24 @@ exports.commands = {
                 }
         },
 
+stafflist: function (target, room, user, connection) {
+var rankLists = {};
+for (var userid in Users.usergroups) {
+var group = Users.usergroups[userid].substr(0, 1);
+if (!rankLists[group]) rankLists[group] = [];
+rankLists[group].push(userid);
+}
+var buffer = [];
+Object.keys(rankLists).sort(function (a, b) {
+return Config.groups[b].rank - Config.groups[a].rank;
+}).forEach(function (r) {
+buffer.push(Config.groups[r].name + "s (" + r + "):\n" + rankLists[r].sort().join(", "));
+});
+if (!buffer.length) return this.sendReply("This server has no staff.");
+connection.popup(buffer.join("\n\n"));
+},
+
+
         unafk: 'back',
         back: function(target, room, user) {
                 var awaynames = ['- ⓈⓁⒺⒺⓅⒾⓃⒼ', '- ⒺⒶⓉⒾⓃⒼ', '- ⓈⓉⓊⒹⓎⒾⓃⒼ', '- ⒶⓃⒾⓂⒺ', '- ⒼⒶⓂⒾⓃⒼ', ' - ⒶⓌⒶⓎ'];
